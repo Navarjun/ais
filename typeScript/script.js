c4ce6bd0c6a62e705e6bf71c8b303aa931b58477
Kini Luo <kini1993129@gmail.com>
2016-12-16 19:17:38 -0500

    add new folder
    add new folder typedScript

---
+++ typeScript/script.js
@@ -0,0 +1,10 @@
+    
+
+
+    $(".typed").typed({
+    strings:["The <strong>ROBOTS</strong> are coming","for our <strong>JOBS</strong>","Or <strong>ARE THEY ?</strong>"],
+    typeSpeed: 100,
+    cursorChar: "",
+    startDelay: 500,
+    backDelay:400
+    });
