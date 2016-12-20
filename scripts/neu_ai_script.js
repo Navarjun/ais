f8991bcd78ad3f96e39503508357df6473d64a2c
Navarjun <navarjunsingh@gmail.com>
2016-12-20 14:19:28 -0500

    Updated slide framework

--- scripts/neu_ai_script.js
+++ scripts/neu_ai_script.js
@@ -5,164 +5,189 @@ AIS.data = {
     Gender: [{label: "Male", countStart: 0, countEnd: 83}, {label: "Female", countStart: 83, countEnd: 167}]
   }
 };
+AIS.basicButtons = ["Big Picture", "Gender", "Age", "Work", "Income"]
 
 // init controller
-AIS.controller = new ScrollMagic.Controller();
-AIS.buttons = ["Gender", "Age"];
-AIS.draw = function(dataCategory, info){
-
-  var x = AIS.plot.selectAll(".people")
-    .data(d3.range(0, 167, 1), function(d){return d;});
-  var padding = (AIS.canvasHeight/13)*(0.6), radius = (AIS.canvasHeight/13)*(0.35);
-
-  var enter = x.enter()
-    .append("circle")
-    .classed("people", true)
-    .attr("id", function(d){return "people-"+d;})
-    .style("opacity", 0)
-    .attr("r", radius)
-    .attr("cx", function(_, i){
-      var pos = parseInt(i/13) * (radius+padding);
-      d3.select(this).attr("abs-x", pos + AIS.canvasWidth/2 - (padding+radius)*5.5)
-      return pos + AIS.canvasWidth/2 - (padding+radius)*5.7;
-    })
-    .attr("cy", function(_, i){
-      d3.select(this).attr("abs-y", (i%13) * (radius+padding))
-      return (i%13) * (radius+padding);
-    })
-    .transition()
-    .delay(function(_, i){return i*10;})
-    .duration(100)
-    .style("opacity", 1);
-
-  x = x.merge(enter);
-
-  var data = AIS.data[dataCategory][info];
-  if (data) {
-    data.forEach(function(d,i){
-      var positive = (i) >= data.length/2 ? (1) : (-1);
-      AIS.plot.selectAll(".people")
-        .data(d3.range(d.countStart, d.countEnd, 1), function(d){return d;})
-        .transition().duration(500)
-        .attr("cx", function(_, i){
-            return (+d3.select(this).attr("abs-x")) + positive*100;
-        });
-
-      var label = AIS.svg.select("#label-"+i);
-      if (!label.node()) {
-        label = AIS.svg.append("text")
-          .attr("class", d.label)
-          .classed("label", true)
-          .attr("id", "label-"+i)
-          .style("opacity", 0)
-      }
-      var x = (+AIS.plot.select("#people-"+d.countStart).attr("abs-x")) + positive*100;
-      label
-        .attr("x", x)
-        .attr("y", AIS.canvasHeight+3)
-        .transition().duration(500)
-        .style("opacity", 1)
-        .text(d.label)
-    })
-  }
-}
+AIS.controller = new ScrollMagic.Controller({
+		globalSceneOptions:{
+			triggerHook:'onLeave'
+		}
+	});
 
 AIS.setup = function() {
-  AIS.svg = d3.select(".canvas")
-    .style("height", window.innerHeight/3)
-    .style("width", window.innerWidth)
-    .style("opacity", 0);
-  AIS.plot = AIS.svg.append("g")
-    .attr("transform", "translate(10, 10)");
-  AIS.canvasHeight = window.innerHeight/3 - 20;
-  AIS.canvasWidth = window.innerWidth - 20;
-
-  AIS.buttonsDiv = d3.select(".button-div")
-    .style("height", "30px")
-    .style("bottom", window.innerHeight/2.8)
-    .selectAll("button")
-    .data(AIS.buttons)
-    .enter()
-    .append("button")
-    .attr("id", function(d){return d;})
-    .classed("btn", true)
-    .classed("btn-default", true)
-    .html(function(d){return d;})
-    .style("opacity", 0)
-    .on("click", function(d){
-      d3.selectAll(".button-div button")
-        .classed("btn-primary", false)
-        .classed("btn-default", true);
-      d3.select(this)
-        .classed("btn-primary", true)
-        .classed("btn-default", false);
-      AIS.draw("total", d3.select(this).attr("id"));
+  AIS.scene1 = new ScrollMagic.Scene({
+		duration:document.getElementById('scene-1').clientHeight, //controlled by height of the #scene-1 <section>, as specified in CSS
+		triggerElement:'#scene-1',
+		reverse:true //should the scene reverse, scrolling up?
+	})
+  .addIndicators()
+	.on('enter',function(){
+		//what happens when we 'enter' the scene i.e. #scene-1 reaches the top of the screen
+		console.log('Enter Scene 1');
+		// d3.select('#plot').transition().style('background','red');
+    $("#scene-1-typed").typed({
+      strings:["The <strong>ROBOTS</strong> are coming","for our <strong>JOBS</strong>","Or <strong>ARE THEY ?</strong>"],
+      typeSpeed: 10,
+      cursorChar: "",
+      startDelay: 500,
+      backDelay:400
     });
+	})
+	.addTo(AIS.controller);
 
-  var i = 1;
-  while(true){
-    var slideID = "#slide-"+i;
-    var slide = d3.select(slideID);
-    if (slide.node()) {
-      var j = 1;
-      while(true) {
-        var divID = "#div-"+j;
-        var div = slide.select(divID);
-        if (div.node()) {
-          if (!(i == 1 && j == 1)) {
-            div.style("opacity", 0);
-          } else {
-            div.style("opacity", 1);
-          }
-          div.style("margin-top", j == 1 ? 0 :(window.innerHeight/2));
 
-          if (d3.select(slideID+" "+"#div-"+(j+1)).node()) {
-            new ScrollMagic.Scene({
-              duration: window.innerHeight/2,
-              offset:10,
-              triggerElement: slideID+" "+divID
-            }).triggerHook(0.4)
-            .setTween(new TimelineMax()
-          			.add([
-          				TweenMax.to(slideID+" "+"#div-"+(j+1), 0.5, {opacity: 1}),
-                  TweenMax.to(slideID+" "+divID, 0.5, {opacity: 0})
-          			]))
-              // .addIndicators({name: "slide-"+i+" : div-"+j})
-              .addTo(AIS.controller);
-          } else {
-            new ScrollMagic.Scene({
-              duration: window.innerHeight/2,
-              offset:10,
-              triggerElement: slideID+" "+divID
-            }).triggerHook(0.2)
-            .setTween(new TimelineMax()
-          			.add([
-          				TweenMax.to("#slide-"+(i+1)+" "+"#div-1", 0.5, {opacity: 1}),
-                  TweenMax.to(slideID+" "+divID, 0.5, {opacity: 0})
-          			]))
-              // .addIndicators({name: "slide-"+(i+1)+" : div-1"})
-              .addTo(AIS.controller)
-              .on("start enter", function(event){
-                if (i == 3 && j == 2) {
-                  AIS.draw("total");
-                  AIS.buttonsDiv
-                    .transition().duration(500)
-                    .style("opacity", event.type == "start" && event.scrollDirection == "REVERSE" ? 0 : 1);
-                  AIS.svg
-                    .transition().duration(500)
-                    .style("opacity", event.type == "start" && event.scrollDirection == "REVERSE" ? 0 : 1);
-                }
-              });
-          }
-        } else break;
-        j++;
+  var svg2 = d3.select("#scene-2-svg");
+  svg2.attr("width", svg2.node().parentNode.clientWidth)
+    .attr("height", 280);
+
+  AIS.scene2 = new ScrollMagic.Scene({
+		duration:document.getElementById('scene-2').clientHeight, //controlled by height of the #scene-1 <section>, as specified in CSS
+		triggerElement:'#scene-2',
+		reverse:true //should the scene reverse, scrolling up?
+	})
+  .addIndicators()
+	.on('enter',function(){
+		//what happens when we 'enter' the scene i.e. #scene-1 reaches the top of the screen
+		console.log('Enter Scene 2');
+		// d3.select('#plot').transition().style('background','red');
+    $("#scene-2-typed").typed({
+      strings:["What do they make of robot revolution?"],
+      typeSpeed: 10,
+      cursorChar: "",
+      startDelay: 500,
+      backDelay:400,
+      callback: function() {
+        // paragraph DOM elements
+        var ps = d3.select("#scene-2-button-div")
+          .selectAll("p")
+          .data(AIS.basicButtons)
+          .enter()
+          .append("p")
+          .append("button")
+          .attr("class", function(d){ return d == "Big Picture" ? "btn scene-2-btn btn-primary" : "btn scene-2-btn btn-default"; })
+          .attr("id", function(d){ return d.toLowerCase().replace(" ", "-"); })
+          .attr("scene", 2)
+          .text(function(d){ return d; })
+          .style("opacity", 0);
+          ps.on("click", function(d){
+            d3.selectAll(".scene-2-btn")
+              .classed("btn-primary", false).classed("btn-default", true);
+            d3.select(this)
+              .classed("btn-primary", true).classed("btn-default", false);
+          });
+          ps.transition().duration(500)
+          .style("opacity", 1);
+
+        var polygons = svg2.selectAll("polygon")
+          .data(d3.range(0, 100, 1), function(d){ return d; })
+          .enter()
+          .append("polygon")
+          .attr("points", AIS.genderMan)
+          .attr("transform", function(d, i){
+            var x = i%10 * 40 + (parseInt(i/10)%2 == 0 ? 0 : 20);
+            var y = parseInt(i/10) * 20;
+
+            return "translate("+x+","+y+"), scale(0.3)";
+          })
+          .style("opacity", 0)
+          .transition().duration(500)
+          .style("opacity", 1);;
       }
-      slide.style("padding-top", (window.innerHeight/2))
-        // .style("padding-bottom", (window.innerHeight/2));
-    } else break;
-    i++;
-  }
-  d3.select("body").style("height", window.innerHeight*i);
+    });
+	})
+	.addTo(AIS.controller);
+
+  var svg3left = d3.select("#scene-3-svg-left");
+  svg3left.attr("width", svg2.node().parentNode.clientWidth)
+    .attr("height", 280);
+  var svg3right = d3.select("#scene-3-svg-right");
+  svg3right.attr("width", svg2.node().parentNode.clientWidth)
+    .attr("height", 280);
+  AIS.scene3 = new ScrollMagic.Scene({
+		duration:document.getElementById('scene-3').clientHeight, //controlled by height of the #scene-1 <section>, as specified in CSS
+		triggerElement:'#scene-3',
+		reverse:true //should the scene reverse, scrolling up?
+	})
+  .addIndicators()
+	.on('enter',function(){
+		//what happens when we 'enter' the scene i.e. #scene-1 reaches the top of the screen
+		console.log('Enter Scene 3');
+		// d3.select('#plot').transition().style('background','red');
+    var ps = d3.select("#scene-3-button-div")
+      .selectAll("button")
+      .data(AIS.basicButtons)
+      .enter()
+      .append("button")
+      .attr("class", function(d){ return d == "Big Picture" ? "btn scene-3-btn btn-primary" : "btn scene-3-btn btn-default"; })
+      .attr("id", function(d){ return d.toLowerCase().replace(" ", "-"); })
+      .attr("scene", 3)
+      .text(function(d){ return d; })
+      .style("opacity", 0);
+      ps.on("click", function(d){
+        d3.selectAll(".scene-3-btn")
+          .classed("btn-primary", false).classed("btn-default", true);
+        d3.select(this)
+          .classed("btn-primary", true).classed("btn-default", false);
+      });
+      ps.transition().duration(500)
+      .style("opacity", 1);
+
+    $("#scene-3-typed-left").typed({
+      strings:["<span class='highlight'>58%</span> <br/>expect AI and robotics to transform the US economy"],
+      typeSpeed: 10,
+      cursorChar: "",
+      startDelay: 500,
+      backDelay:400,
+      callback: function() {
+        var polygons = svg3left.selectAll("polygon")
+          .data(d3.range(0, 100, 1), function(d){ return d; })
+          .enter()
+          .append("polygon")
+          .attr("points", AIS.genderMan)
+          .attr("transform", function(d, i){
+            var x = i%10 * 40 + (parseInt(i/10)%2 == 0 ? 0 : 20);
+            var y = parseInt(i/10) * 20;
+
+            return "translate("+x+","+y+"), scale(0.3)";
+          })
+          .style("opacity", 0)
+          .transition().duration(500)
+          .style("opacity", function(d, i) {
+            return i < 58 ? 1 : 0.2;
+          });
+
+          $("#scene-3-typed-right").typed({
+            strings:["<strong>Although</strong> <span class='highlight'>48%</span>  <br/>worry that the US workforce will not adapt quickly enough"],
+            typeSpeed: 10,
+            cursorChar: "",
+            startDelay: 500,
+            backDelay:400,
+            callback: function() {
+              var polygons = svg3right.selectAll("polygon")
+                .data(d3.range(0, 100, 1), function(d){ return d; })
+                .enter()
+                .append("polygon")
+                .attr("points", AIS.genderMan)
+                .attr("transform", function(d, i){
+                  var x = i%10 * 40 + (parseInt(i/10)%2 == 0 ? 0 : 20);
+                  var y = parseInt(i/10) * 20;
+
+                  return "translate("+x+","+y+"), scale(0.3)";
+                })
+                .style("opacity", 0)
+                .transition().duration(500)
+                .style("opacity", function(d, i) {
+                  return i < 48 ? 1 : 0.2;
+                });
+            }
+          });
+      }
+    });
+	})
+	.addTo(AIS.controller);
 }
+
+AIS.genderMan = "15.2,26.2 7.5,26.2 3.8,26.2 3.8,30 3.8,33.6 3.8,37.4 3.8,41.1 3.8,44.9 3.8,48.7 3.8,52.4 3.8,56.1 3.8,59.7 3.8,63.5 3.8,67.2 3.8,71 3.8,74.8 7.5,74.8 7.5,78.4 7.5,82.2 7.5,85.9 7.5,89.5 7.5,93.3 7.5,97.1 7.5,100.7 3.8,100.7 0,100.7 0,104.5 3.8,104.5 7.5,104.5 11.3,104.5 11.3,100.7 11.3,97.1 11.3,93.3 11.3,89.5 11.3,85.9 11.3,82.2 11.3,78.4 11.3,74.8 14.9,74.8 18.7,74.8 18.7,78.4 18.7,82.2 18.7,85.9 18.7,89.5 18.7,93.3 18.7,97.1 18.7,100.7 18.7,104.5 22.5,104.5 26.2,104.5 30,104.5 30,100.7 26.2,100.7 22.5,100.7 22.5,97.1 22.5,93.3 22.5,89.5 22.5,85.9 22.5,82.2 22.5,78.4 22.5,74.8 26.2,74.8 26.2,71 26.2,67.2 26.2,63.5 26.2,59.7 26.2,56.1 26.2,52.4 26.2,48.7 26.2,44.9 26.2,41.1 26.2,37.4 26.2,33.6 26.2,30 26.2,26.2 22.5,26.2 15.2,26.2 15.2,22.6 22.5,22.6 22.5,18.8 22.5,15.1 22.5,11.3 22.5,7.7 22.5,3.9 22.5,0.1 18.7,0.1 14.9,0.1 11.3,0.1 7.5,0.1 7.5,3.9 7.5,7.7 7.5,11.3 7.5,15.1 7.5,18.8 7.5,22.6 15.2,22.6";
+
 
 AIS.setup();
